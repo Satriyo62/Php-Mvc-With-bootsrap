c1d2e1c9ed5cd0a96d0496d36a4e53e0fe5f05b4
Yunan <satriyoyunan@gmail.com>
2025-10-20 19:11:07 +0700

    Update About.php

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,11 +1,12 @@
 <?php
 
 class About extends Controller {
-    public function index($nama = 'Muhsyam Fahriel', $NIM = '244311021', $Prodi = 'TRPL'){
+    public function index($nama = 'Satriyo Wicaksono Yunan Mubarok', $NIM = '244311027', $Prodi = 'TRPL'){
         $data['nama'] = $nama;
         $data['NIM'] = $NIM;
         $data['Prodi'] = $Prodi;
 
         $this->view('about/index', $data);
     }
+
 }
